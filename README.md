bdde56bce1b5e51d798e76d761e75fdca5392cd0
Petar Ivanov <peter.ivanov89@gmail.com>
2012-09-28 15:21:36 +0400

    prism source + property file + test1

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ microcin
 ========
 
 Microcin bacterial system simulation and verification 
+
+PRISM model checker: http://www.prismmodelchecker.org/
---
+++ microcin.pm
@@ -0,0 +1,49 @@
+// microcin simulation
+
+// model is a CTMC
+ctmc
+
+//-----------------------------------------------------------------------
+// modules for the different components
+
+const int MAX_MCC_IN  = 100;
+const int MAX_MCC_OUT = 100;
+
+const double synthesis_rate    ;
+const double output_rate       ;
+const double input_rate        = 1e-10;
+const double inactivation_rate;
+
+module microcin
+  McCin  : [0..MAX_MCC_IN]  init 0;
+  McCout : [0..MAX_MCC_OUT] init 0;
+  
+  [synthesis]     McCin<MAX_MCC_IN -> synthesis_rate : (McCin'=McCin+1);
+  [output]        McCin>0 -> McCin*output_rate : (McCin'=McCin-1) & (McCout'=min(McCout+1, MAX_MCC_OUT));
+  //[input]         McCin>0 & McCout>0 -> McCin*input_rate : (McCin'=min(McCin+1, MAX_MCC_IN)) & (McCout'=McCout-1);
+  [inactivation]  McCin>0 -> McCin*inactivation_rate : (McCin'=McCin-1);
+endmodule
+
+rewards "synthesis"
+  [synthesis]     true : 1;
+endrewards
+
+//rewards "inactivation"
+//  [inactivation]  true : 1;
+//endrewards
+
+rewards "time"
+  true : 1;
+endrewards
+
+rewards "out_over_in"
+  McCin>0 : McCout / McCin;
+endrewards
+
+rewards "in"
+  true : McCin;
+endrewards
+
+rewards "out"
+  true : McCout;
+endrewards
